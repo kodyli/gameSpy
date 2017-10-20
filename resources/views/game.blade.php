711c5a0dd8f2184de2bca70afa3c4a292adff707
Kody Li <yansan.li@foxmail.com>
2017-10-20 10:39:21 -0700

    add google analytics

--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -18,7 +18,14 @@
 		@include('./layouts/enemies', ['enemies'=>$game->getRedTeam()])
 		@include('./layouts/allies', ['allies'=>$game->getBlueTeam()])
 	</div>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-108360018-1"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
 
-
+	  gtag('config', 'UA-108360018-1');
+	</script>
 </body>
 </html>
