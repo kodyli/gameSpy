051053a72e912840b865764ff23193547d639300
Kody Li <yansan.li@foxmail.com>
2017-10-20 10:38:44 -0700

    add google analytics

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -47,6 +47,14 @@
 		</div> -->
 
 	</form>
-	</div>	
+	</div>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-108360018-1"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+	  gtag('config', 'UA-108360018-1');
+	</script>
 </body>
 </html>
