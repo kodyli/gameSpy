a739630cdefd4ad4a9bec5f29904123d2fa37ad5
Kody Li <yansan.li@foxmail.com>
2017-10-20 21:56:02 -0700

    update key

--- src/kody/lol/LolModel.php
+++ src/kody/lol/LolModel.php
@@ -5,7 +5,7 @@ use App\Src\Kody\Model;
 
 abstract class LolModel extends Model {
 	protected $domain ='https://{region}.api.riotgames.com';
-	protected $apiKey='RGAPI-6a977c8b-e9c6-416f-bda5-03962d85e118';
+	protected $apiKey='RGAPI-a7ae6748-98dc-4318-9db1-a71cf9f46678';
 	
 	public function __construct(){
 		//partent::__construct();
